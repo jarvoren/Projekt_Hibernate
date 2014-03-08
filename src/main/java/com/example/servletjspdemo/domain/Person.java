b77e52a5c06d1f2382a3bb72c2004ddbb7c5e9e5
student <student@virtualbox>
2014-03-08 19:12:53 +0100

    dodano hibernate

--- src/main/java/com/example/servletjspdemo/domain/Person.java
+++ src/main/java/com/example/servletjspdemo/domain/Person.java
@@ -1,7 +1,15 @@
 package com.example.servletjspdemo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
 	
+	@Id
+	@GeneratedValue
+	private int id;
 	private String firstName = "unknown";
 	private int yob = 1900;
 	
